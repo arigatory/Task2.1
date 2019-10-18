114ad7e2ee80d3a7e5104105f3278c47c94df8a5
arigatory <arigatory@gmail.com>
2019-10-18 17:26:41 +0300

    to String realize

--- Task2.1/Complex.cs
+++ Task2.1/Complex.cs
@@ -8,13 +8,44 @@ namespace Task2._1
 {
     class Complex
     {
-        private double re;
-        private double im;
-
+        private readonly double epsilon = 1e-4;
         public double Re { get; set; }
         public double Im { get; set; }
         public double Phi { get; set; }
         public double R { get; set; }
+
+        public Complex(double re = 0, double im = 0)
+        {
+            Re = re;
+            Im = im;
+        }
+
+        public override string ToString()
+        {
+            string res = "";
+            if (Math.Abs(Re) < epsilon && Math.Abs(Im) < epsilon)
+            {
+                res = "0";
+            } else if (Math.Abs(Re) < epsilon)
+            {
+                res = $"{Im:0.##}i";
+            } else if (Math.Abs(Im) < epsilon)
+            {
+                if (Re < 0)
+                    res += "-";
+                res += $"{Re:0.##}";
+            }
+            else
+            {
+                res += $"{Re:0.##} ";
+                if (Im < 0)
+                    res += (Math.Abs(Im) - 1 < epsilon) ? $"+ i" : $"- {Im:0.##}i";
+                else
+                    res += (Math.Abs(Im) - 1 < epsilon) ? $"+ i" : $"- {Im:0.##}i";
+            }
+
+            return res;
+        }
 
         public double Abs
         {
--- Task2.1/Program.cs
+++ Task2.1/Program.cs
@@ -10,6 +10,14 @@ namespace Task2._1
     {
         static void Main(string[] args)
         {
+            Complex c1 = new Complex(-1,-1);
+            Complex c2 = new Complex(3,4);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c1.Abs);
+            Console.WriteLine(c2.Abs);
+           
             Console.ReadLine();
         }
     }
